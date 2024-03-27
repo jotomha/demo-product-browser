2220a7d172321b3faa3c0158a52943aaf95fdd60
osluup <jonjonth1@gmail.com>
2024-03-27 10:12:19 -0700

    Quick base app layout for lg screens and above

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Heading, List, ListItem } from "@chakra-ui/react";
+import "./App.css";
+import ProductDisplay from "./components/ProductDisplay";
+import FilterCategory from "./components/FilterCategory";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface ProductRequest {
+  category: string | null;
+  search: string | null;
+  page: number;
+  prodPerPage: number;
 }
 
-export default App
+function App() {
+  const [productQuery, setProductQuery] = useState<ProductRequest>({
+    category: null,
+    search: null,
+    page: 1,
+    prodPerPage: 30,
+  });
+
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav nav nav" "filter products cart"',
+      }}
+      templateColumns={{
+        base: "10vw 80vw 10vw",
+      }}
+      templateRows={{
+        base: "10vh 90vh",
+      }}
+    >
+      <GridItem area="nav">nav bar</GridItem>
+      <GridItem area="filter" bgColor="tomato" padding="5%">
+        <Heading fontSize="2xl" marginBottom={3} margin={5}>
+          Filters
+        </Heading>
+        <List>
+          <ListItem>
+            <FilterCategory
+              selectedCategory={productQuery.category}
+              onSelectCategory={(t) =>
+                setProductQuery({ ...productQuery, category: t })
+              }
+            />
+          </ListItem>
+        </List>
+      </GridItem>
+      <GridItem area="products" bgColor="skyblue" w="100%">
+        <ProductDisplay page={productQuery.page} perPage={productQuery.page} />
+      </GridItem>
+      <GridItem area="cart" bgColor="olive">
+        this the cart
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
