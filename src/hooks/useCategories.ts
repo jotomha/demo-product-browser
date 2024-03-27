67821da41eb0fc1ce4df372365dc980ef68db738
osluup <jonjonth1@gmail.com>
2024-03-27 10:11:38 -0700

    Updated category hook

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,12 +1,7 @@
 import useApi from "./useApi";
-import { ProdObj } from "./useProducts";
 
-// When not supplied a "category" string, requests all categories
-// When supplied a string, returns a list of products under that category
-const useCategories = (category?: string) => {
-  if (category) {
-    return useApi<ProdObj>(`/products/category/${category}`);
-  }
+// requests all categories from API
+const useCategories = () => {
   return useApi<string>(`/products/categories`);
 };
 
