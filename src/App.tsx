1b65af04e1c9830ee3bec1f1a43ad67fffd4b2c1
osluup <jonjonth1@gmail.com>
2024-03-28 13:08:38 -0700

    General changes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Heading,
   List,
   ListItem,
+  Text,
 } from "@chakra-ui/react";
 import "./App.css";
 import ProductDisplay from "./components/ProductDisplay";
@@ -13,6 +14,9 @@ import FilterCategory from "./components/FilterCategory";
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import PageSelector from "./components/PageSelector";
+import CartDisplay, { Cart, CartProduct } from "./components/CartDisplay";
+import { getCart } from "./hooks/useCart";
+import { productToPost } from "./services/productToPost";
 
 export interface ProductRequest {
   category: string | null;
@@ -29,26 +33,38 @@ function App() {
     prodPerPage: 16,
   });
 
+  const [cart, setCart] = useState<Cart>({} as Cart);
+
+  /* were you able to save cart states on the server, this could be used to initially load the cart with a certain id. Keep dependencies empty to only 
+  run this initial loading once.
+  useEffect(() => {
+    getCart(setCart, []);
+  }, []); */
+
   return (
     <Grid
       templateAreas={{
         base: '"nav nav nav" "filter products cart"',
       }}
       templateColumns={{
-        base: "10vw 80vw 10vw",
+        base: "150px 0.7fr 0.3fr",
       }}
       templateRows={{
-        base: "10vh 90vh",
+        base: "0.1fr 1fr",
       }}
+      height="100%"
+      width="100%"
     >
-      <GridItem area="nav">
+      <GridItem area="nav" padding="10px" maxHeight="100px">
+        <Heading marginBottom="8px">Demo Product Browser</Heading>
         <SearchBar
           onSearch={(t) => setProductQuery({ ...productQuery, search: t })}
           placeholder="Search for specific products"
-          width="100vw"
+          width="100%"
         />
       </GridItem>
-      <GridItem area="filter" bgColor="tomato" padding="5%">
+
+      <GridItem area="filter" padding="5%">
         <Heading fontSize="2xl" marginBottom={3} margin={5}>
           Filters
         </Heading>
@@ -63,7 +79,15 @@ function App() {
           </ListItem>
         </List>
       </GridItem>
-      <GridItem area="products" bgColor="skyblue" w="100%" padding="20px">
+
+      <GridItem
+        area="products"
+        h="100%"
+        w="100%"
+        padding="20px"
+        overflowY="hidden"
+        paddingBottom="100px"
+      >
         <Heading fontSize="2xl" margin="10px">
           Products
         </Heading>
@@ -83,10 +107,48 @@ function App() {
             setProductQuery({ ...productQuery, prodPerPage: t })
           }
         />
-        <ProductDisplay prodReq={productQuery}></ProductDisplay>
+        <ProductDisplay
+          prodReq={productQuery}
+          onAddItem={(itemId: number, itemQuantity: number) => {
+            //This absolutely horrendous chunk of code here adds the new product to a list. I wasn't sure how to complete this map of CartProduct objects
+            // to CartPost objects inline getCart() so I just opted for this, which also handles some null checking. There's definitely a better way here;
+            // potentially something like combining the CartProduct and CartPost objects into one object to avoid this mapping business.
+            let postProducts = productToPost(cart.products);
+            if (postProducts) {
+              postProducts.push({ id: itemId, quantity: itemQuantity });
+            } else {
+              postProducts = [{ id: itemId, quantity: itemQuantity }];
+            }
+            getCart(setCart, postProducts);
+          }}
+        ></ProductDisplay>
       </GridItem>
-      <GridItem area="cart" bgColor="olive">
-        this the cart
+
+      <GridItem
+        area="cart"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        padding="10px"
+      >
+        <Heading fontSize="2xl" margin="10px">
+          Your Cart
+        </Heading>
+        <CartDisplay
+          onChangeCart={(productList: CartProduct[]) =>
+            getCart(setCart, productToPost(productList))
+          }
+          cart={cart}
+        ></CartDisplay>
+        <Box
+          display="flex"
+          flexDir="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Text>Total price: ${cart.total ? cart.total : 0}</Text>
+          <Button onClick={() => getCart(setCart, [])}>Clear Cart</Button>
+        </Box>
       </GridItem>
     </Grid>
   );
