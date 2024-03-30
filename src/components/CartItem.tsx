4c71b6c2bef58f517c19ec5f629bc8f56f220849
osluup <jonjonth1@gmail.com>
2024-03-30 14:37:22 -0700

    cart item formatting

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -9,10 +9,6 @@ interface Props {
 }
 
 const CartItem = ({ product, onDelete, onUpdateQuantity }: Props) => {
-  //Cart item can do 2 things:
-  //  1. Update quantity of any given product.
-  //  2. Fully remove item from cart.
-
   return (
     <Card
       flexDirection="row"
@@ -25,38 +21,46 @@ const CartItem = ({ product, onDelete, onUpdateQuantity }: Props) => {
       <Button backgroundColor="#f1807e" onClick={onDelete}>
         X
       </Button>
-      <Text textAlign="center" lineHeight="1">
-        {product.title}
-      </Text>
-      <Box display="flex" flexDir="row" alignItems="center">
-        <Button
-          height="20px"
-          width="20px"
-          margin="0px"
-          padding="0px"
-          paddingBottom="3px"
-          onClick={() => onUpdateQuantity(Math.max(1, product.quantity - 1))}
-        >
-          &lt;
-        </Button>
-        <Text margin="0px 5px 0px 5px">{product.quantity}</Text>
-        <Button
-          height="20px"
-          width="20px"
-          margin="0px"
-          padding="0px"
-          paddingBottom="3px"
-          onClick={() => onUpdateQuantity(product.quantity + 1)}
-        >
-          &gt;
-        </Button>
+      <Box
+        display="flex"
+        flexDir={{ base: "column", sm: "row" }}
+        justifyContent={"space-between"}
+        alignItems="center"
+        minWidth="80%"
+      >
+        <Text textAlign="center" lineHeight="1">
+          {product.title}
+        </Text>
+        <Box display="flex" flexDir="row" alignItems="center">
+          <Button
+            height="20px"
+            width="20px"
+            margin="0px"
+            padding="0px"
+            paddingBottom="3px"
+            onClick={() => onUpdateQuantity(Math.max(1, product.quantity - 1))}
+          >
+            &lt;
+          </Button>
+          <Text margin="0px 5px 0px 5px">{product.quantity}</Text>
+          <Button
+            height="20px"
+            width="20px"
+            margin="0px"
+            padding="0px"
+            paddingBottom="3px"
+            onClick={() => onUpdateQuantity(product.quantity + 1)}
+          >
+            &gt;
+          </Button>
+        </Box>
+        <DiscountedText
+          normalPrice={product.price}
+          discountPerc={product.discountPercentage}
+          discountPrice={product.discountedPrice}
+          showDiscount={false}
+        />
       </Box>
-      <DiscountedText
-        normalPrice={product.price}
-        discountPerc={product.discountPercentage}
-        discountPrice={product.discountedPrice}
-        showDiscount={false}
-      />
     </Card>
   );
 };
