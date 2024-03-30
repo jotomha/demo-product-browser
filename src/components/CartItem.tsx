cff6fce0052b3d78a45a48fb9b3365b13b07a3ad
osluup <jonjonth1@gmail.com>
2024-03-30 12:33:25 -0700

    Formatting change

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Text } from "@chakra-ui/react";
+import { Box, Button, Card, Text } from "@chakra-ui/react";
 import { CartProduct } from "./CartDisplay";
 import SearchBar from "./SearchBar";
 import { stringToNumber } from "../services/stringToNumber";
@@ -20,45 +20,44 @@ const CartItem = ({ product, onDelete, onUpdateQuantity }: Props) => {
       flexDirection="row"
       alignItems="center"
       overflow="hidden"
-      justifyContent="space-evenly"
+      justifyContent="space-between"
+      padding="10px"
       width="100%"
     >
       <Button backgroundColor="#f1807e" onClick={onDelete}>
         X
       </Button>
-      <Text>{product.title}</Text>
-      <Button
-        height="20px"
-        width="20px"
-        margin="0px"
-        padding="0px"
-        paddingBottom="3px"
-        onClick={() => onUpdateQuantity(Math.max(1, product.quantity - 1))}
-      >
-        &lt;
-      </Button>
-      <SearchBar
-        onSearch={(t: string) =>
-          onUpdateQuantity(Math.max(1, stringToNumber(t, product.quantity)))
-        }
-        placeholder={product.quantity.toString()}
-        width="20px"
-      ></SearchBar>
-      <Button
-        height="20px"
-        width="20px"
-        margin="0px"
-        padding="0px"
-        paddingBottom="3px"
-        onClick={() => onUpdateQuantity(product.quantity + 1)}
-      >
-        &gt;
-      </Button>
+      <Text textAlign="center" lineHeight="1">
+        {product.title}
+      </Text>
+      <Box display="flex" flexDir="row" alignItems="center">
+        <Button
+          height="20px"
+          width="20px"
+          margin="0px"
+          padding="0px"
+          paddingBottom="3px"
+          onClick={() => onUpdateQuantity(Math.max(1, product.quantity - 1))}
+        >
+          &lt;
+        </Button>
+        <Text margin="0px 5px 0px 5px">{product.quantity}</Text>
+        <Button
+          height="20px"
+          width="20px"
+          margin="0px"
+          padding="0px"
+          paddingBottom="3px"
+          onClick={() => onUpdateQuantity(product.quantity + 1)}
+        >
+          &gt;
+        </Button>
+      </Box>
       <DiscountedText
         normalPrice={product.price}
         discountPerc={product.discountPercentage}
         discountPrice={product.discountedPrice}
-        showDiscount={true}
+        showDiscount={false}
       />
     </Card>
   );
