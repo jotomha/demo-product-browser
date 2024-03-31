bd2b6ddb72b45a45c06af5346753a8d641f2444c
osluup <jonjonth1@gmail.com>
2024-03-30 21:43:06 -0700

    Comments

--- src/services/productToPost.ts
+++ src/services/productToPost.ts
@@ -1,6 +1,10 @@
 import { CartProduct } from "../components/CartDisplay";
 import { CartPost } from "../hooks/useCart";
 
+//Converts a list of "product" objects to objects that work with the getCart hook (CartPost)
+//Probably didn't need to be its own function but it's just so much nicer to be able to call this
+//and I wasn't sure how many times I was going to need this functionality (I think I only ended up
+//using it once but oh well)
 export const productToPost = (products: CartProduct[]) => {
   return products?.map((cp) => {
     return { id: cp.id, quantity: cp.quantity } as CartPost;
--- src/services/stringToNumber.ts
+++ src/services/stringToNumber.ts
@@ -1,3 +1,4 @@
+//Parses an integer from provided string, and returns default if input was not a number.
 export const stringToNumber = (inp: string, def: number) => {
   if (isNaN(parseInt(inp))) return def;
   else return parseInt(inp);
